34a0845b083115430350eceeba3713e6fb76f619
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-13 00:17:14 -0700

    throws error when no solution is found

--- src/javascript/two_sum_test_.js
+++ src/javascript/two_sum_test_.js
@@ -13,17 +13,17 @@
                     }
                 }
             }
-            throw new Error("Target Not Found");
+            throw new Error("Error: Target Not Found");
         }
         
-        it("find two sums in an array that equal the target", function() {
+        it("find the sum of two numbers in an array that equal the target", function() {
             assert.deepEqual(twoSumsBruteForce(9, [2, 7, 11, 15]), [0, 1]);
         });
         
-        // it("returns an error if no combination is found that equals the target", function() {
-        //     assert.throws(function() {
-        //         twoSumsBruteForce(9, [3, 7, 11, 15]);
-        //     });
-        // });
+        it("returns an error if no combination is found that equals the target", function() {
+            assert.throws(function() {
+                twoSumsBruteForce(9, [3, 7, 11, 15]);
+            });
+        });
     });
 }());
