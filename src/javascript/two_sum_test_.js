ebe096dc3f094cb6cb772315a53b408112a6b58a
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-15 10:21:36 -0700

    twoSumsHashTwoPass()

--- src/javascript/two_sum_test_.js
+++ src/javascript/two_sum_test_.js
@@ -1,3 +1,4 @@
+/* global Map */
 (function() {
 
     "use strict";
@@ -15,9 +16,23 @@
             }
             throw new Error("Error: Target Not Found");
         }
+
+        function twoSumsHashTwoPass(target, nums) {
+            var map = new Map();
+            var i;
+            for (i = 0; i < nums.length; i++) {
+                map.set(nums[i], i);
+            }
+            for (i = 0; i < nums.length; i++) {
+                if (map.get(target - nums[i]) !== undefined) {
+                    return [i, map.get(target - nums[i])];
+                }
+            }
+        }
         
         it("find the sum of two numbers in an array that equal the target", function() {
             assert.deepEqual(twoSumsBruteForce(9, [2, 7, 11, 15]), [0, 1]);
+            assert.deepEqual(twoSumsHashTwoPass(9, [2, 7, 11, 15]), [0, 1]);
         });
         
         it("returns an error if no combination is found that equals the target", function() {
