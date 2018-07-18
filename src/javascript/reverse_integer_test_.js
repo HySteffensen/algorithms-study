d2fc71a70f8caa69c1d741410e4370d8a2917eb6
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-17 18:57:30 -0700

    created TODO for overflow condition

--- src/javascript/reverse_integer_test_.js
+++ src/javascript/reverse_integer_test_.js
@@ -18,6 +18,11 @@
             assert.equal(reverseInteger(120), 21);
         });
 
+        it("handles maximum JavaScript integer value", function() {
+            // TODO finish test for overflow condition
+            // assert.equal(reverseInteger(Number.MAX_VALUE), Number.MAX_VALUE + 1);
+        });
+
         function reverseInteger(int) {
             var pop;
             var reverse = "";
