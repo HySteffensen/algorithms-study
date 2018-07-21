e26304ce1c2587ca2a66f9b98fe991d49c479b8c
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-21 16:13:17 -0700

    Added tests to check when no input is passed

--- src/javascript/14_Longest_Common_Prefix.js
+++ src/javascript/14_Longest_Common_Prefix.js
@@ -8,9 +8,16 @@
     describe("Longest Common Prefix", function() {
         it("Finds the longest common prefix between words", function() {
             assert.equal(longestCommonPrefix(["flower","flow","flight"]), "fl");
+            assert.equal(longestCommonPrefix([]), "");
+            assert.throws(function() {
+                longestCommonPrefix();
+            }, "Error: No input was passed.");
         });
 
         function longestCommonPrefix(wordList) {
+            if (wordList === undefined) throw "Error: No input was passed.";
+            if (!wordList.length) return '';
+
             for (var i = 0; i < wordList[0].length; i++) {
                 for (var word of wordList) {
                     if (word[i] !== wordList[0][i]) {
