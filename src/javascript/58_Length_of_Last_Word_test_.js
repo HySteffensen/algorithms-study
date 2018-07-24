cc475bc929c3135914e1c00c7d7b6dc34f76c0e5
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-24 13:06:24 -0700

    added error handling

--- src/javascript/58_Length_of_Last_Word_test_.js
+++ src/javascript/58_Length_of_Last_Word_test_.js
@@ -8,8 +8,19 @@
         it("finds the length of the last word", function() {
             assert.equal(lengthOfLastWord("Hello World"), 5);
         });
+
+        it("returns 0 if there are no words", function() {
+            assert.equal(lengthOfLastWord(""), 0);
+        });
+
+        it("thows an error message if there is no input", function() {
+            assert.throws(function() {
+                lengthOfLastWord();
+            }, "Error: No input provided.");
+        });
         
         function lengthOfLastWord(str) {
+            if (str === undefined) throw new Error("Error: No input provided.");
             return str.split(" ").pop().length;
         }
 
