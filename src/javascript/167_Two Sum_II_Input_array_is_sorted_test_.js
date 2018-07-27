17f44988522187cdbb3204853c880e48642e021c
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-27 10:10:39 -0700

    throws error when no solution is found

--- src/javascript/167_Two Sum_II_Input_array_is_sorted_test_.js
+++ src/javascript/167_Two Sum_II_Input_array_is_sorted_test_.js
@@ -5,18 +5,25 @@
 
     describe("167. Two Sum II - Input array is sorted", function() {
         it("finds the sum of two numbers in an array that equals a target", function() {
-            function twoSumII(numSet, target) {
-                for (var i = 0; i < numSet.length; i++) {
-                    for (var j = 0; j < numSet.length; j++) {
-                        if ((numSet[i] + numSet[j]) === target) {
-                            return [i + 1, j + 1];
-                        }
+            assert.deepEqual(twoSumII([2,7,11,15], 9), [1, 2]);
+        });
+        
+        it("throws an error when no solution is found", function() {
+            assert.throws(function() {
+                twoSumII([2,8,11,15], 9);
+            }, "No solution found.");
+        });
+
+        function twoSumII(numSet, target) {
+            for (var i = 0; i < numSet.length; i++) {
+                for (var j = 0; j < numSet.length; j++) {
+                    if ((numSet[i] + numSet[j]) === target) {
+                        return [i + 1, j + 1];
                     }
                 }
             }
-
-            assert.deepEqual(twoSumII([2,7,11,15], 9), [1, 2]);
-        });
+            throw new Error("No solution found.");
+        }
 
     });
 
