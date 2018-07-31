2022e3e2bf217e3185bdcd1e7712c586f802b7c4
HySteffensen <hyrum.steffensen@gmail.com>
2018-07-31 00:02:07 -0700

    two pointers

--- src/javascript/167_Two Sum_II_Input_array_is_sorted_test_.js
+++ src/javascript/167_Two Sum_II_Input_array_is_sorted_test_.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 (function() {
     "use strict";
 
@@ -6,13 +7,33 @@
     describe("167. Two Sum II - Input array is sorted", function() {
         it("finds the sum of two numbers in an array that equals a target", function() {
             assert.deepEqual(twoSumII([2,7,11,15], 9), [1, 2]);
+            assert.deepEqual(twoSumTwoPointers([2,7,11,15], 9), [1, 2]);
         });
         
         it("throws an error when no solution is found", function() {
             assert.throws(function() {
                 twoSumII([2,8,11,15], 9);
             }, "No solution found.");
+            assert.deepEqual(twoSumTwoPointers([2,8,11,15], 9), []);
         });
+
+        function twoSumTwoPointers (numbers, target) {
+            var lo = 0;
+            var hi = numbers.length -1;
+
+            while (lo < hi) {
+                var sum = numbers[lo] + numbers[hi];
+
+                if (sum === target) {
+                    return [lo + 1, hi + 1];
+                } else if (hi > lo) {
+                    hi = hi - 1;
+                } else {
+                    lo = lo + 1;
+                }
+            }
+            return [];
+        }
 
         function twoSumII(numSet, target) {
             for (var i = 0; i < numSet.length; i++) {
@@ -26,16 +47,5 @@
         }
 
     });
-
-
-
-
-
-
-
-
-
-
-
 
 }());
