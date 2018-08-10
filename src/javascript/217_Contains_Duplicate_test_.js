42e26fe6e6344f72e19f7c3b53e1f7e094b24e9c
HySteffensen <hyrum.steffensen@gmail.com>
2018-08-09 18:45:27 -0700

    refactored function name

--- src/javascript/217_Contains_Duplicate_test_.js
+++ src/javascript/217_Contains_Duplicate_test_.js
@@ -5,15 +5,15 @@
 
     describe("217. Contains Duplicate", function() {
         it("finds duplicates in an array", function() {
-            assert.equal(findDuplicate([1,2,3,1]), true);
-            assert.equal(findDuplicate([1,1,1,3,3,4,3,2,4,2]), true);
+            assert.equal(findDuplicateBruteForce([1,2,3,1]), true);
+            assert.equal(findDuplicateBruteForce([1,1,1,3,3,4,3,2,4,2]), true);
         });
 
         it("returns false if no duplicate found", function() {
-            assert.equal(findDuplicate([1,2,3,4]), false);
+            assert.equal(findDuplicateBruteForce([1,2,3,4]), false);
         });
 
-        function findDuplicate(nums) {
+        function findDuplicateBruteForce(nums) {
             for (var i = 0; i < nums.length; i++) {
                 for (var j = 0; j < nums.length; j++) {
                     if ((nums[i] === nums[j]) && (i !== j)) {
