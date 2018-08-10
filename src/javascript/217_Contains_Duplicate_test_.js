6a4cc42b62e88774fca28f9e36e3b3f55f231f66
HySteffensen <hyrum.steffensen@gmail.com>
2018-08-09 18:43:40 -0700

    contains duplicate

---
+++ src/javascript/217_Contains_Duplicate_test_.js
@@ -0,0 +1,28 @@
+(function() {
+    "use strict";
+
+    var assert = require("./assert");
+
+    describe("217. Contains Duplicate", function() {
+        it("finds duplicates in an array", function() {
+            assert.equal(findDuplicate([1,2,3,1]), true);
+            assert.equal(findDuplicate([1,1,1,3,3,4,3,2,4,2]), true);
+        });
+
+        it("returns false if no duplicate found", function() {
+            assert.equal(findDuplicate([1,2,3,4]), false);
+        });
+
+        function findDuplicate(nums) {
+            for (var i = 0; i < nums.length; i++) {
+                for (var j = 0; j < nums.length; j++) {
+                    if ((nums[i] === nums[j]) && (i !== j)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    });
+
+}());
